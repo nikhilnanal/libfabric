b648a38bea889ab1efce8881f9300aed315eb69b
nikhilnanal <nikhilnanal1@gmail.com>
2019-07-24 15:22:14 -0700

    changed to BuildNo from BUILD_NO in Build-MPICH-Benchmarks.sh
    Signed-off-by: nikhilnanal <nikhilnanal1@gmail.com>

--- contrib/Intel/JenkinsBuildScripts/Build-MPICH-Benchmarks.sh
+++ contrib/Intel/JenkinsBuildScripts/Build-MPICH-Benchmarks.sh
@@ -3,26 +3,26 @@
         BuildNo=$2
 		    (
 		    cd $WORKSPACE
-		    mkdir -p /home/build/ofi-Install/libfabric/$BranchName/$BUILD_NO/mpich
-		    mkdir -p /home/build/ofi-Install/libfabric/$BranchName/$BUILD_NO/mpich_build/  && cd /home/build/ofi-Install/libfabric/$BranchName/$BUILD_NO/mpich_build/
-		    /home/build/scm/mpich/configure --disable-oshmem --enable-fortran=no --prefix="/home/build/ofi-Install/libfabric/$BranchName/$BUILD_NO/mpich/" --with-libfabric="/home/build/ofi-Install/libfabric/$BranchName/$BUILD_NO/"
+		    mkdir -p /home/build/ofi-Install/libfabric/$BranchName/$BuildNo/mpich
+		    mkdir -p /home/build/ofi-Install/libfabric/$BranchName/$BuildNo/mpich_build/  && cd /home/build/ofi-Install/libfabric/$BranchName/$BuildNo/mpich_build/
+		    /home/build/scm/mpich/configure --disable-oshmem --enable-fortran=no --prefix="/home/build/ofi-Install/libfabric/$BranchName/$BuildNo/mpich/" --with-libfabric="/home/build/ofi-Install/libfabric/$BranchName/$BuildNo/"
 		    make clean
 		    make install -j32
 		    )
 
 		    #build mpi stress test with mpich
 		    (
-		    mkdir -p /home/build/ofi-Install/libfabric/$BranchName/$BUILD_NO/mpich/stress && cd /home/build/ofi-Install/libfabric/$BranchName/$BUILD_NO/mpich/stress 
+		    mkdir -p /home/build/ofi-Install/libfabric/$BranchName/$BuildNo/mpich/stress && cd /home/build/ofi-Install/libfabric/$BranchName/$BuildNo/mpich/stress 
 		    /home/build/jenkinsbuild/workspace/libfabrics-pipbuild/mpich/bin/mpicc -lz /home/build/scm/wfr-mpi-tests/mpi_stress/mpi_stress.c -o /home/build/jenkinsbuild/workspace/libfabrics-pipbuild/mpich/stress/mpi_stress
 		    )
 		    #build osu benchmarks with mpich
 		    (
-		    mkdir -p /home/build/ofi-Install/libfabric/$BranchName/$BUILD_NO/mpich/osu && cd /home/build/ofi-Install/libfabric/$BranchName/$BUILD_NO/mpich/osu
-		    export CC=/home/build/ofi-Install/libfabric/$BranchName/$BUILD_NO/mpich/bin/mpicc
-		    export CXX=/home/build/ofi-Install/libfabric/$BranchName/$BUILD_NO/mpich/bin/mpicxx
+		    mkdir -p /home/build/ofi-Install/libfabric/$BranchName/$BuildNo/mpich/osu && cd /home/build/ofi-Install/libfabric/$BranchName/$BuildNo/mpich/osu
+		    export CC=/home/build/ofi-Install/libfabric/$BranchName/$BuildNo/mpich/bin/mpicc
+		    export CXX=/home/build/ofi-Install/libfabric/$BranchName/$BuildNo/mpich/bin/mpicxx
 		    export CFLAGS="-I/home/build/scm/osu-micro-benchmarks-5.5/util/"
 		    export LD_LIBRARY_PATH=""
-		    /home/build/scm/osu-micro-benchmarks-5.5/configure --prefix=/home/build/ofi-Install/libfabric/$BranchName/$BUILD_NO/mpich/osu
+		    /home/build/scm/osu-micro-benchmarks-5.5/configure --prefix=/home/build/ofi-Install/libfabric/$BranchName/$BuildNo/mpich/osu
 		    make -j4
 		    make install
 		    )
